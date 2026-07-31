81d5c9bed4271def0a944960b22348fb36af3b8d
yanboww <wywkaixin10@gmail.com>
2026-07-30 20:31:46 -0400

    finishing touches + new assets

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
+// Hamburger menu
 const toggle = document.querySelector(".nav-toggle");
 const nav = document.querySelector("#site-nav");
 
 toggle.addEventListener("click", () => {
     const isOpen = nav.classList.toggle("is-open");
     toggle.setAttribute("aria-expanded", isOpen);
+})
+
+//footer icon animation
+
+const footerIcon = document.querySelector(".footer-icon");
+
+footerIcon?.addEventListener("click", () => {
+    footerIcon.getAnimations().forEach((animation) => animation.cancel());
+
+    footerIcon.animate(
+        [
+            {transform: "rotate(0deg)"},
+            {transform: "rotate(360deg)"}
+        ],
+        {
+            duration: 600,
+            easing: "linear"
+        }
+    );
 })
